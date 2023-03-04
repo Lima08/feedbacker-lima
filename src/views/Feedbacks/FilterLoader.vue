92b4f8f1e17872630236a9ab0f6111560a4bceee
LimaBagy08 <joao.lima@bagy.com.br>
2023-03-04 18:33:09 -0300

    Feat: Adiciona componente filterLoader

---
+++ src/views/Feedbacks/FilterLoader.vue
@@ -0,0 +1,37 @@
+<script setup>
+import ContentLoader from '@/components/ContentLoader/index.vue'
+</script>
+
+<template>
+  <content-loader
+    class="flex flex-col items-center rounded"
+    width="100%"
+    height="200px"
+    animation-duration="0s"
+  >
+    <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="30px"
+      animation-duration="2.7s"
+    />
+    <content-loader
+      class="mt-3 rounded"
+      width="80%"
+      height="30px"
+      animation-duration="2.5s"
+    />
+    <content-loader
+      class="mt-3 rounded"
+      width="80%"
+      height="30px"
+      animation-duration="2.5s"
+    />
+    <content-loader
+      class="mt-3 rounded"
+      width="70%"
+      height="30px"
+      animation-duration="2.3s"
+    />
+  </content-loader>
+</template>
