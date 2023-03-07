46a3637b84dba6732b7eb189490e47ae346b8361
LimaBagy08 <joao.lima@bagy.com.br>
2023-03-06 23:10:02 -0300

    feat: Adiciona componente badge

---
+++ src/components/FeedbackCard/Badge.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+  type: {
+    require: true,
+    type: String,
+  },
+})
+
+const label = computed(() => {
+  if (props.type === 'ISSUE') return 'problema'
+  if (props.type === 'IDEA') return 'ideia'
+  return 'outros'
+})
+
+const classColor = computed(() => {
+  if (props.type === 'ISSUE') return 'brand-danger'
+  if (props.type === 'IDEA') return 'brand-warning'
+  return 'brand-graydark'
+})
+</script>
+
+<template>
+  <span class="rounded-full p-2 text-xs font-black text-white uppercase" :class="classColor">
+    {{ label }}
+  </span>
+</template>
