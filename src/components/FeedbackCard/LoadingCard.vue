d63f8cf003f2064c434e0fea4f86dc354762ddc4
LimaBagy08 <joao.lima@bagy.com.br>
2023-03-06 23:10:33 -0300

    feat: Adiciona componente FeedbackCardLoading

---
+++ src/components/FeedbackCard/LoadingCard.vue
@@ -0,0 +1,42 @@
+<script setup>
+import ContentLoader from '@/components/ContentLoader/index.vue'
+</script>
+
+<template>
+  <div class="bg-gray-100 rounded-md">
+    <div class="flex justify-between px-4">
+      <content-loader
+        class="mt-3 rounded"
+        width="20%"
+        height="30px"
+        animation-duration="1s"
+      />
+      <content-loader
+        class="mt-3 rounded"
+        width="20%"
+        height="30px"
+        animation-duration="1.5s"
+      />
+    </div>
+
+    <content-loader
+      class="mt-3 rounded"
+      width="100%"
+      height="30px"
+      animation-duration="2s"
+    />
+
+    <content-loader
+      class="mt-3 rounded"
+      width="60%"
+      height="30px"
+      animation-duration="2s"
+    />
+    <content-loader
+      class="mt-3 rounded"
+      width="70%"
+      height="30px"
+      animation-duration="1.7s"
+    />
+  </div>
+</template>
