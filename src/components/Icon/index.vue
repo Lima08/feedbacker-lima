87f33d782d7ad803e527fadb3b43256220f7ea5d
LimaBagy08 <joao.lima@bagy.com.br>
2023-03-07 22:09:05 -0300

    feat: Adiciona icone arrowDown

--- src/components/Icon/index.vue
+++ src/components/Icon/index.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Loading from './Loading.vue'
 import Copy from './Copy.vue'
+import ArrowDown from './ArrowDown.vue'
 
 const props = defineProps(['name', 'size', 'color'])
 const icons = {
   Loading,
   Copy,
+  ArrowDown,
 }
 </script>
 <template>
